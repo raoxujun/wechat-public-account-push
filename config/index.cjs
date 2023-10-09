a61e894cd1cb63514dd01a65ee7656cf70a13531
raoxujun <111725527+raoxujun@users.noreply.github.com>
2023-10-09 21:29:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oJ1va6ZHz0V3ZMkFNWqDPLzJJU8s',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '6Ic3kEND4atuOjv9qSNyTlxrSlOyQvxBhyOWghmECxM',
+      useTemplateId: 'oJ1va6ZHz0V3ZMkFNWqDPLzJJU8s',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-11',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '6Ic3kEND4atuOjv9qSNyTlxrSlOyQvxBhyOWghmECxM',
+  CALLBACK_TEMPLATE_ID: ' 	_x7lT-aOKwgY54Tec7a0q0-R9TPB2yuxC6sO08uuE2I',
 
   CALLBACK_USERS: [
     {
